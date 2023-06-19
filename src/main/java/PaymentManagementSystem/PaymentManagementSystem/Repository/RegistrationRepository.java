1b055ef203d7a9a364f1287098530000b4c64543
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-19 10:53:43 +0400

    add attribute in RegistrationService

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/RegistrationRepository.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/RegistrationRepository.java
@@ -2,7 +2,9 @@ package PaymentManagementSystem.PaymentManagementSystem.Repository;
 
 import PaymentManagementSystem.PaymentManagementSystem.Model.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Integer> {
 
 }
