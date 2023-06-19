197e268b277e88712758c48ee15d65d83ec544c5
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-19 11:47:04 +0400

    Complete the class of realationShip

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/PaymentHistory.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/PaymentHistory.java
@@ -1,4 +1,28 @@
 package PaymentManagementSystem.PaymentManagementSystem.Model;
 
-public class PaymentHistory {
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Data
+@Entity
+public class PaymentHistory extends BaseEntity{
+
+    @OneToOne
+    @JoinColumn(name = "registerId", referencedColumnName = "id") // FK
+    Registration registration;
+
+    @OneToOne
+    @JoinColumn(name = "paymentId" , referencedColumnName = "id")
+    PaymentMethod paymentMethod;
+
+    @OneToOne
+    @JoinColumn(name = "TransactionId" , referencedColumnName = "id")
+    Transaction transaction;
+    @OneToOne
+    @JoinColumn(name = "paymentProcessId" , referencedColumnName = "id")
+    PaymentProcess paymentProcess;
+
 }
