6514fb3f563dccd83f28cb18244180585a733c67
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-19 10:36:44 +0400

    add attribute of class baseEntity

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/BaseEntity.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/BaseEntity.java
@@ -1,4 +1,17 @@
 package PaymentManagementSystem.PaymentManagementSystem.Model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+@Getter
+@Setter
+@MappedSuperclass
 public class BaseEntity {
+    Date createDate;
+    Date updateDate;
+    Boolean isActive;
+    
 }
