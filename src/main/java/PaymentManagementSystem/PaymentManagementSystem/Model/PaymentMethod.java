1b30729de1f51aa3092d3444623c14af95eec2f3
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-19 11:53:11 +0400

    Add attribute of class PaymentProcess

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/PaymentMethod.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/PaymentMethod.java
@@ -1,4 +1,26 @@
 package PaymentManagementSystem.PaymentManagementSystem.Model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Setter
+@Getter
+@Entity
+@Data
+@Table(name = "PaymentMethod")
 public class PaymentMethod {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer paymentId;
+    Long amount;
+    String currency;
+    PaymentMethod paymentMethod;
+    Long cardNumber;
+    Date expriationDate;
+
 }
