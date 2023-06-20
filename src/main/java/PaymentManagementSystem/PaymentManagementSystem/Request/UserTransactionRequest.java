c7171f7c421a7838d33128d0ed88604f77acf25e
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-20 11:54:48 +0400

    try to make all part is correct with out any error

---
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Request/UserTransactionRequest.java
@@ -0,0 +1,30 @@
+package PaymentManagementSystem.PaymentManagementSystem.Request;
+
+import PaymentManagementSystem.PaymentManagementSystem.Model.UserTransaction;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Data
+@Getter
+@Setter
+public class UserTransactionRequest {
+    Long transactionId;
+    double amount;
+    String currency;
+    String status;
+
+    public UserTransaction convertToUSerTransaction() {
+        Date nowDate = new Date();
+        UserTransaction usertransaction = new UserTransaction();
+        usertransaction.setTransactionId(this.getTransactionId());
+        usertransaction.setAmount(this.getAmount());
+        usertransaction.setCurrency(this.getCurrency());
+        usertransaction.setStatus(this.getStatus());
+        usertransaction.setCreateDate(nowDate);
+        usertransaction.setIsActive(true);
+        return usertransaction;
+    }
+}
