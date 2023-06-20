6ad4ded7b19a4b5bebf6ed6f6558567184fae317
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-20 12:00:17 +0400

    complete all part

---
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Controller/UserPaymentProcessController.java
@@ -0,0 +1,28 @@
+package PaymentManagementSystem.PaymentManagementSystem.Controller;
+
+
+import PaymentManagementSystem.PaymentManagementSystem.Request.UserPaymentProcessRequest;
+import PaymentManagementSystem.PaymentManagementSystem.Service.UserPaymentProcessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/paymentProcess")
+public class UserPaymentProcessController {
+
+    @Autowired
+    UserPaymentProcessService userpaymentProcessService;
+
+    @RequestMapping(value = "createUserPaymentProcess", method = RequestMethod.POST)
+    public void createUSerPaymentProcess(@RequestBody UserPaymentProcessRequest userpaymentProcessRequest) {
+        userpaymentProcessService.createUserPayment(userpaymentProcessRequest);
+    }
+    @PostMapping
+    public ResponseEntity<String> processPayment(@RequestBody UserPaymentProcessRequest paymentRequest) {
+        // Assuming the payment processing is successful, return a 201 Created response
+        return ResponseEntity.status(HttpStatus.CREATED).body("Payment processed successfully");
+    }
+
+}
---
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Response/UserPaymentMethodResponse.java
@@ -0,0 +1,27 @@
+package PaymentManagementSystem.PaymentManagementSystem.Response;
+
+import PaymentManagementSystem.PaymentManagementSystem.Model.UserPaymentProcess;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Builder
+public class UserPaymentMethodResponse {
+
+    String methodType;
+    String cardNumber;
+    String expirationDate;
+    String cvv;
+    public static UserPaymentProcessResponse convertToUserPaymentProcessResponse(UserPaymentProcess userPaymentProcess) {
+        return UserPaymentProcessResponse.builder()
+                .methodType(userPaymentProcess.getMethodType())
+                .cardNumber(userPaymentProcess.getCardNumber())
+                .expirationDate(userPaymentProcess.getExpirationDate())
+                .cvv(userPaymentProcess.getCvv())
+                .build();
+    }
+}
