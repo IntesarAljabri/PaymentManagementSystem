4c9751f2c51556f7f99b47f27b4b348f4d9dbf4c
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-21 15:26:36 +0400

    write the query to used in service class

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentMethodRepository.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentMethodRepository.java
@@ -1,6 +1,4 @@
 package PaymentManagementSystem.PaymentManagementSystem.Repository;
-
-
 import PaymentManagementSystem.PaymentManagementSystem.Model.UserPaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPaymentMethodRepository extends JpaRepository<UserPaymentMethod, Integer> {
+
     @Query("DELETE FROM UserPaymentMethod p WHERE p.id = :id")
     void deleteAllPaymentMethod(@Param("id") Long id);
 }
