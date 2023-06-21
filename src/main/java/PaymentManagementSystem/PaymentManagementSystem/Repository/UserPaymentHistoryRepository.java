3a3fa654aecf2f59f797150c559b1af06d25d037
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-21 15:23:31 +0400

    check the all code inside

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentHistoryRepository.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentHistoryRepository.java
@@ -1,12 +1,14 @@
 package PaymentManagementSystem.PaymentManagementSystem.Repository;
-
 import PaymentManagementSystem.PaymentManagementSystem.Model.UserPaymentHistory;
+import PaymentManagementSystem.PaymentManagementSystem.Model.UserPaymentProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface UserPaymentHistoryRepository extends JpaRepository<UserPaymentHistory, Long> {
-    List<UserPaymentHistory> findByUserId(Long userId);
+    @Query("SELECT u FROM UserPaymentProcess u WHERE u.id= :id")
+    List<UserPaymentHistory> findByUserId(@Param("id") Long userId);
 }
