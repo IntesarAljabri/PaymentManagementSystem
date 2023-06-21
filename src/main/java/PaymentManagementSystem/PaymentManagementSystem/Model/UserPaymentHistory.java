241b85a2a181b742564b36abe898a913dd4052ac
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-21 15:21:26 +0400

    change the methods and make it clean and easy to used

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/UserPaymentHistory.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Model/UserPaymentHistory.java
@@ -1,9 +1,7 @@
 package PaymentManagementSystem.PaymentManagementSystem.Model;
-
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.Date;
 
@@ -17,10 +15,10 @@ public class UserPaymentHistory extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
-
      String paymentId;
      Long amount;
      String currency;
      Date date;
      String status;
+
 }
