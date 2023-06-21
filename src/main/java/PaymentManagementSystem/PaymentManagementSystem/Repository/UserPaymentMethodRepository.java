8dedc24c55c180282c03ec03d6284f1942522256
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-21 16:49:47 +0400

    delete the query

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentMethodRepository.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentMethodRepository.java
@@ -3,12 +3,12 @@ import PaymentManagementSystem.PaymentManagementSystem.Model.UserPaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories
 @Repository
 public interface UserPaymentMethodRepository extends JpaRepository<UserPaymentMethod, Integer> {
-    @Modifying
-    @Query("UPDATE UserPaymentMethod p SET p.paymentStatus = :newStatus WHERE p.id = :id")
-    void updatePaymentMethodStatus(@Param("id") Long id, @Param("newStatus") String newStatus);
+
 }
