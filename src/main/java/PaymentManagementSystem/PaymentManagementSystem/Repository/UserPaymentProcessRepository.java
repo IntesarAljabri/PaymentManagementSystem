a031ae63738f370be13cfcc6c7e7e2de99a97915
Maan94919411* <intesaraljabri99@gmail.com>
2023-06-21 16:51:21 +0400

    change the query and add @EnableJpaRepositories

--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentProcessRepository.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserPaymentProcessRepository.java
@@ -2,9 +2,11 @@ package PaymentManagementSystem.PaymentManagementSystem.Repository;
 import PaymentManagementSystem.PaymentManagementSystem.Model.UserPaymentProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+@EnableJpaRepositories
 @Repository
 public interface UserPaymentProcessRepository extends JpaRepository<UserPaymentProcess, Integer> {
     @Query("SELECT u FROM UserPaymentProcess u WHERE u.id= :id")
--- src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserSubscriptionRepository.java
+++ src/main/java/PaymentManagementSystem/PaymentManagementSystem/Repository/UserSubscriptionRepository.java
@@ -2,9 +2,11 @@ package PaymentManagementSystem.PaymentManagementSystem.Repository;
 import PaymentManagementSystem.PaymentManagementSystem.Model.UserSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories
 @Repository
 public interface UserSubscriptionRepository extends JpaRepository<UserSubscription, Integer> {
     @Query("SELECT s FROM UserSubscription s WHERE s.id = :id")
